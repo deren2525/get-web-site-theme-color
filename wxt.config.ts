58d687f692c7bda49cc8e1aa693822f5def4515d
deren2525 <deren2525@gmail.com>
2026-07-18 18:44:09 +0900

    v2.2.1

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   manifest: {
     name: '__MSG_Name__',
     description: '__MSG_Description__',
-    version: '2.2.0',
+    version: '2.2.1',
     default_locale: 'en',
     action: {
       default_title: '__MSG_Name__',
